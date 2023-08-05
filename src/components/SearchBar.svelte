b25d0e5b6f6b8dc0bcc04afd97e12b251363e6fe
us1aaron <aaron.li@stonybrook.edu>
2023-08-05 11:53:49 -0400

    search bar add

--- src/components/SearchBar.svelte
+++ src/components/SearchBar.svelte
@@ -1,10 +1,22 @@
 <script lang="ts">
-
 </script>
 
-<div class="searchBarContainer">
-    <span class="material-icons searchIcon">search</span>
-    <div>
-        <input class="search" type="text" placeholder="search for song/artist"/>
+<div class="searchContainer">
+	<div class="searchBarContainer">
+		<span class="material-icons searchIcon">search</span>
+		<input class="search" type="text" placeholder="search for song/artist" />
+	</div>
+	<div class="searchResults">
+        <div class="result">
+            <span class="material-icons">mic_external_on</span>
+            <div class="resultContent">Sam Smith</div>
+        </div>
+        <div class="result">
+            <span class="material-icons">music_note</span>
+            <div class="resultContent">Sam Song</div>
+        </div>
     </div>
 </div>
+
+
+<!-- <span class="material-icons">music_note</span> -->
